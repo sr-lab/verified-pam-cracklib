55d1d93c693c90902e4bedc4cea51832ce8a75c0
Saul Johnson <saul.a.johnson@gmail.com>
2017-02-17 18:26:22 +0000

    Add Extraction script

---
+++ src/Extraction.v
@@ -0,0 +1,13 @@
+Require Import Pam.Functions.MinClass.
+Require Import Pam.Functions.Sequence.
+Require Import Pam.Functions.Palindrome.
+Require Import Pam.Functions.WordCheck.
+
+(* The following imports are useful for extracting Haskell code. *)
+Require Import ExtrHaskellBasic.
+Require Import ExtrHaskellNatInt.
+Require Import ExtrHaskellZInteger.
+Require Import ExtrHaskellString.
+
+Extraction Language Haskell.
+Extraction "PalindromeGenerated.hs" boolToNat palindrome.
