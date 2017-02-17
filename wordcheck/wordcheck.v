fade514939a18eac3ef3437e6715efe229d51fd9
Saul Johnson <saul.a.johnson@gmail.com>
2017-02-17 16:07:12 +0000

    Add wordcheck function

---
+++ wordcheck/wordcheck.v
@@ -0,0 +1,44 @@
+Require Import Coq.Arith.EqNat.
+Require Import Coq.Init.Nat.
+Require Import Coq.Strings.Ascii.
+Require Import Coq.Strings.String.
+
+(* The following imports are useful for extracting Haskell code. *)
+Require Import ExtrHaskellBasic.
+Require Import ExtrHaskellNatInt.
+Require Import ExtrHaskellZInteger.
+Require Import ExtrHaskellString.
+
+(* Returns true if two strings are equivalent, otherwise returns false. *)
+Fixpoint string_eq(s1 s2 : string) : bool :=
+  match s1, s2 with
+  | EmptyString, EmptyString => true
+  | String c1 s1', String c2 s2' => andb
+    (beq_nat (nat_of_ascii c1) (nat_of_ascii c2)) (string_eq s1' s2')
+  | _, _ => false
+  end.
+
+(* Returns true if a string contains a substring, otherwise returns false. *)
+Fixpoint contains(h n: string) : bool :=
+  match h with 
+  | String c s => 
+      if length h <? length n then
+        false
+      else if string_eq (substring 0 (length n) h) n then
+        true
+      else
+        contains s n
+  | EmptyString => eqb (length n) 0
+  end.
+
+(* Returns a string, reversed. *)
+Fixpoint str_reverse(s : string) : string :=
+  match s with
+  | EmptyString => EmptyString
+  | String c s' => append (str_reverse s') (String c EmptyString)
+  end.
+
+(* Returns true if a string contains a substring or a reversed version of that 
+   substring, otherwise returns false. *)
+Fixpoint wordcheck(s w : string) : bool :=
+  orb (contains s w) (contains (str_reverse s) w).
