8e82af93f854d46b7e0b89e7d9009bf4f242b8a8
Saul Johnson <saul.a.johnson@gmail.com>
2017-02-17 17:14:43 +0000

    Get src folder set up

---
+++ configure.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+# Use Coq utility to generate build script
+coq_makefile -f Make -o Makefile-gen
---
+++ src/BoolExtensions.v
@@ -0,0 +1,6 @@
+(* Converts the boolean value true to the natural number 1, false to 0. *)
+Fixpoint bool_to_nat(b: bool) : nat :=
+  match b with
+  | true => 1
+  | false => 0
+  end.
