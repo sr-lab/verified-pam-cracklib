35a3a7b17603a605c1d61db7794125f7e48f5e3c
Saul Johnson <saul.a.johnson@gmail.com>
2017-02-17 18:32:51 +0000

    Extract all four pure functions

--- src/Extraction.v
+++ src/Extraction.v
@@ -10,4 +10,4 @@ Require Import ExtrHaskellZInteger.
 Require Import ExtrHaskellString.
 
 Extraction Language Haskell.
-Extraction "PalindromeGenerated.hs" palindrome.
+Extraction "PamGenerated.hs" palindrome wordcheck minclass sequence.
