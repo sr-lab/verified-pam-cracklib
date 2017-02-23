5749f899d32542c82c2e4c7d7fffa98113d2f325
Saul Johnson <saul.a.johnson@gmail.com>
2017-02-23 15:18:20 +0000

    Wire in C harness

---
+++ src/PamInterface.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+ 
+module PamInterface where
+ 
+import Foreign.C.Types
+import Foreign.C.String
+
+-- Unsure about this one
+import System.IO.Unsafe
+
+-- Import functional code that can be generated
+import PamGenerated
+
+-- TODO: can I get rid of the unsafePerformIO?
+palindrome_hs :: CString -> CInt
+palindrome_hs = fromIntegral . boolToNat . palindrome . unsafePerformIO . peekCString
+ 
+foreign export ccall palindrome_hs :: CString -> CInt
--- src/configure.sh
+++ src/configure.sh
@@ -1,2 +1,3 @@
 ( echo "-R . Pam" ; find . -name "*.v" -print ) > _CoqProject
-coq_makefile -f _CoqProject -o Makefile
+coq_makefile -f _CoqProject -o Makefile_Coq
+make -f Makefile_Coq
