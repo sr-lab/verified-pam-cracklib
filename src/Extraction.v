9e7181c31ebed98053d704bf01b34e3bec988c87
Saul Johnson <saul.a.johnson@gmail.com>
2017-02-23 17:13:02 +0000

    Add similar function

--- src/Extraction.v
+++ src/Extraction.v
@@ -3,7 +3,7 @@ Require Import Pam.Functions.MinClass.
 Require Import Pam.Functions.Sequence.
 Require Import Pam.Functions.Palindrome.
 Require Import Pam.Functions.WordCheck.
-Require Import Pam.Functions.Levenshtein.
+Require Import Pam.Functions.Similar.
 
 (* The following imports are useful for extracting Haskell code. *)
 Require Import ExtrHaskellBasic.
@@ -12,4 +12,4 @@ Require Import ExtrHaskellZInteger.
 Require Import ExtrHaskellString.
 
 Extraction Language Haskell.
-Extraction "PamGenerated.hs" bool_to_nat palindrome wordcheck minclass sequence consecutive levenshtein_distance.
+Extraction "PamGenerated.hs" bool_to_nat palindrome wordcheck minclass sequence consecutive similar.
---
+++ src/Functions/Similar.v
@@ -0,0 +1,7 @@
+Require Import Coq.Strings.String.
+
+Require Import Pam.Functions.Levenshtein.
+
+Definition similar (a b : string) (d : nat) : bool :=
+  negb (ltb (levenshtein_distance a b) m).
+  
