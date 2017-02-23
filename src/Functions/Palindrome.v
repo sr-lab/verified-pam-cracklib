4a9c2e122c97b20519bc07a2a8b1bedb36ae10fa
Saul Johnson <saul.a.johnson@gmail.com>
2017-02-23 15:27:56 +0000

    Remove contrived natural number palindrome func

--- src/Functions/Palindrome.v
+++ src/Functions/Palindrome.v
@@ -1,12 +1,8 @@
 Require Import Coq.Strings.String.
 
-Require Import Pam.BoolExtensions.
 Require Import Pam.StringExtensions.
 
 (* Returns true if a string is a palindrome, otherwise returns false. *)
-Definition palindromeb (s : string) : bool :=
+Definition palindrome (s : string) : bool :=
   string_eqb s (string_reverse s).
-
-(* Returns 1 if a string is a palindrome, otherwise returns 0. *)
-Definition palindrome (s : string) : nat :=
-  bool_to_nat (palindromeb s).
+  
