3f620f1791875f9bde9be0038a17af8724861d00
Saul Johnson <saul.a.johnson@gmail.com>
2017-02-23 15:04:04 +0000

    Neaten up Levenshtein function

--- src/Functions/Levenshtein.v
+++ src/Functions/Levenshtein.v
@@ -2,38 +2,29 @@ Require Import Coq.Arith.EqNat.
 Require Import Coq.Strings.Ascii.
 Require Import Coq.Strings.String.
 
-Definition bool_to_nat (b : bool) : nat :=
-  match b with
-    | true => 1
-    | false => 0
-  end.
+Require Import Pam.BoolExtensions.
+Require Import Pam.AsciiExtensions.
 
-Definition unpack_option_ascii (a : option ascii) : ascii :=
-  match a with
-    | None => zero
-    | Some a => a
-  end.
-
+(* The Levenshtein indicator function that will return 0 if the character at 
+   each given position is each given string is equal, otherwise returns 1. *)
 Definition indicator (a b : string) (i j : nat) : nat :=
-  bool_to_nat (negb (beq_nat
-    (nat_of_ascii (unpack_option_ascii (get (i - 1) a)))
-    (nat_of_ascii (unpack_option_ascii (get (j - 1) b))))).
+  bool_to_nat (negb (beq_option_ascii (get (i - 1) a) (get (j - 1) b))).
 
+( The Levenshtein distance function. Returns the number of insertions, deletions
+  and substitutions required to go from one string to another. *)
 Fixpoint levenshtein (a b : string) (i j n : nat) : nat :=
   match i, j, n with
     | O, _, _ | _, O, _ | _, _, O => max i j
-    | S i', S j', S n' => 
-      min 
-        ((levenshtein a b i' j n') + 1)
-        (min 
-          ((levenshtein a b i j' n') + 1)
-          ((levenshtein a b i' j' n') + (indicator a b i j)))
+    | S i', S j', S n' => min 
+      ((levenshtein a b i' j n') + 1)
+      (min 
+        ((levenshtein a b i j' n') + 1)
+        ((levenshtein a b i' j' n') + (indicator a b i j)))
   end.
 
-Fixpoint levenshtein_distance (a b : string) : nat :=
+(* Calculates the Levenshtein distance betweem two strings. *)  
+Definition levenshtein_distance (a b : string) : nat :=
   let i := length a in
     let j := length b in
       let n := max i j in
         levenshtein a b i j n.
-
-Compute levenshtein_distance "joaoferreira" "juao".
