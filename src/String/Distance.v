e842b92765cf54344e3d394aa72cfdb8773055e5
Saul Johnson <saul.a.johnson@gmail.com>
2017-03-10 17:24:00 +0000

    Add a bunch of admitted lemmas about LD

--- src/String/Distance.v
+++ src/String/Distance.v
@@ -31,12 +31,41 @@ Definition levenshtein_distance (a b : string) : nat :=
       let n := max i j in
         levenshtein a b i j n.
 
-(* TODO: Lemma - It is always at least the difference of the sizes of the two strings. *)
+(* Returns the length difference between two strings. *)
+Fixpoint string_length_diff (a b : string) : nat :=
+  match a, b with
+    | EmptyString, EmptyString => 0
+    | _, EmptyString => length a
+    | EmptyString, _ => length b
+    | String ca a', String cb b' => string_length_diff a' b'
+  end.
 
-(* TODO: Lemma - It is zero if and only if the strings are equal. *)
+(* TODO: Prove lemma - It is always at least the difference of the sizes of the two strings. *)
+Lemma levenshtein_distance_atleast_length_diff : forall (a b : string),    
+    levenshtein_distance a b >= string_length_diff a b.
+Proof.
+  Admitted.
 
-(* TODO: Lemma - If the strings are the same size, the Hamming distance is an upper bound on the Levenshtein distance. *)
+(* TODO: Prove lemma - It is zero if and only if the strings are equal. *)
+Lemma levenshtein_distance_zero_for_equal_strings : forall (a b : string),
+  a = b <-> levenshtein_distance a b = 0.
+Proof.
+  Admitted.
 
-(* TODO: Lemma - It is at most the length of the longer string. *)
+(* TODO: Prove lemma - If the strings are the same size, the Hamming distance is an upper bound on the Levenshtein distance. *)
+Lemma levenshtein_distance_is_bounded_by_hamming_samelength : forall (a b : string),
+  True. (* TODO: Write lemma. *)
+Proof.
+  Admitted.
 
-(* TODO: Lemma - The Levenshtein distance between two strings is no greater than the sum of their Levenshtein distances from a third string (triangle inequality). *)
+(* TODO: Prove lemma - It is at most the length of the longer string. *)
+Lemma levenshtein_distance_leq_longer_string_length : forall (a b : string),
+   levenshtein_distance a b <= max (length a) (length b).
+Proof.
+  Admitted.
+
+(* TODO: Proove lemma - The Levenshtein distance between two strings is no greater than the sum of their Levenshtein distances from a third string (triangle inequality). *)
+Lemma levenshtein_distance_triangle_equality : forall (a b c : string),
+    levenshtein_distance a b <= (levenshtein_distance a c) + (levenshtein_distance b c).
+Proof.
+  Admitted.
