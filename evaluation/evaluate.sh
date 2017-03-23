bd53ac5268553ef35fb5306334d6f53cc2ac9c45
Saul Johnson <saul.a.johnson@gmail.com>
2017-03-23 13:49:55 +0000

    Process out bad password string in script

--- evaluation/evaluate.sh
+++ evaluation/evaluate.sh
@@ -12,6 +12,10 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 
 	# Result of attempted password change.
 	RESULT=$(expect -f passwd.exp "$USER" "$line")
+	RESULT="${RESULT//New password: /}"
+	RESULT="${RESULT//Retype new password: /}"
+	RESULT="${RESULT/spawn passwd $USER/}"
+	RESULT=$(echo "${RESULT//[$'\r\n']}")
 
 	# Check if password is valid.
 	VALID="true"
