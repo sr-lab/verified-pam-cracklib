64105138e5a78c28791cb94423827a0853196f0e
Saul Johnson <saul.a.johnson@gmail.com>
2017-03-23 10:29:13 +0000

    Print as JSON

--- evaluation/evaluate.sh
+++ evaluation/evaluate.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+COMMA=""
+echo "["
 while IFS='' read -r line || [[ -n "$line" ]]; do
-	printf "%s\n>>>\n" $line
-	expect -f passwd.exp $USER $line
-	printf "\n<<<\n"
+	TIME=$(date +%s%N)
+	RESULT=$(expect -f passwd.exp "$USER" "$line")
+	#echo $RESULT
+	printf "%s{\"password\":\"%s\",\"time\":\"%s\",\"result\":\"%s\"}" "$COMMA" "$line" "$TIME" "$RESULT"
+	COMMA=","
 done < "$1"
-
+echo "]"
