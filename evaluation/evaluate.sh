22ded97926c2cf0ff184b2158fe41addd1738f0b
Saul Johnson <saul.a.johnson@gmail.com>
2017-03-23 13:56:49 +0000

    Space out sentences

--- evaluation/evaluate.sh
+++ evaluation/evaluate.sh
@@ -12,6 +12,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	RESULT="${RESULT//New password: /}"
 	RESULT="${RESULT//Retype new password: /}"
 	RESULT="${RESULT/spawn passwd $USER/}"
+	RESULT="${RESULT/./ .}" # Space out sentences.
 	RESULT=$(echo "${RESULT//[$'\r\n']}")
 
 	# Time in nanoseconds.
