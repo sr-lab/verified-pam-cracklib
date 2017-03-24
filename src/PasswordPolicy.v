eb934c476ee7bd0caaa810658efbc03041635651
Joao F. Ferreira <joao@joaoff.com>
2017-03-24 21:02:05 +0000

    Updated new interface (based on PwdTransition)

--- src/PasswordPolicy.v
+++ src/PasswordPolicy.v
@@ -22,12 +22,14 @@ Definition all_good (old_pwd : option Password) (new_pwd : Password) : CheckerRe
 (* Define password quality policy. *)
 Definition pwd_quality_policy :=
   [
-      diff_from_old_pwd
-    ; not_palindrome
+      diff_from_old_pwd_pt
+    ; not_palindrome_pt
+(*
     ; not_rotated
     ; not_case_changes_only
     ; levenshtein_distance_gt 5
     ; credits_length_check 8
+*)
     (* TODO: confirm that we can remove the following two checkers:
       ; prefix_of_old_pwd
       ; all_good
