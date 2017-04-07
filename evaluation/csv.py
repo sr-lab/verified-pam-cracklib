111a4d30439e9a284868ba5328a7212b3b6fc2ec
Saul Johnson <saul.a.johnson@gmail.com>
2017-04-07 11:59:40 +0100

    Update CSV script to import basic16 results

--- evaluation/csv.py
+++ evaluation/csv.py
@@ -1,6 +1,12 @@
 import json
 
 def import_json(path):
+    """Decodes and returns the JSON object in the file at the specified path.
+	
+    Args:
+        path (str): The path of the file to read.
+		
+    """
     with open(path) as data_file:    
         return json.load(data_file)
         
@@ -10,9 +16,10 @@ results_nodict = import_json('results_nodict.json')['runs']
 results_nodict_mcr = import_json('results_nodict_mcr.json')['runs']
 results_nodict_mcr_fixed = import_json('results_nodict_mcr_fixed.json')['runs']
 results_v_mcr = import_json('results_v_mcr.json')['runs']
+results_v_basic16 = import_json('results_v_basic16.json')['runs']
 
 # Produce CSV.
-results = "Password, Default, Default T, HAPSL, HAPSL T, MCR, MCR T, MCR (fixed), MCR (fixed) T, MCR (HAPSL), MCR (HAPSL) T\n"
+results = "Password, Default, Default T, HAPSL, HAPSL T, MCR, MCR T, MCR (fixed), MCR (fixed) T, MCR (HAPSL), MCR (HAPSL) T, Basic16, Basic16 T\n"
 for i in range(0, 100000):
     results += results_v[i]['password']
     results += ", "
@@ -35,6 +42,10 @@ for i in range(0, 100000):
     results += str(results_v_mcr[i]['valid'])
     results += ", "
     results += str(results_v_mcr[i]['time'])
+    results += ", "
+    results += str(results_v_basic16[i]['valid'])
+    results += ", "
+    results += str(results_v_basic16[i]['time'])
     results += "\n"
 
 # Write CSV out.    
