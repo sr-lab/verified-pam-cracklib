d35a89f6b84b174f8477076ae5fa3ee7f51b1db0
Saul Johnson <saul.a.johnson@gmail.com>
2017-04-25 17:05:23 +0100

    Finish up proof

--- src/Checkers/Basic.v
+++ src/Checkers/Basic.v
@@ -113,14 +113,23 @@ Definition plain_length_check (len : nat) (pt : PasswordTransition) : CheckerRes
   else
     GOODPWD.
 
-(*Lemma plain_length_check_correct : forall (len : nat) (pt : PasswordTransition),
+Lemma plain_length_check_correct : forall (len : nat) (pt : PasswordTransition),
   plain_length_check len pt = GOODPWD <-> is_true (negb (leb (length (new_pwd pt)) len)).
 Proof.
   intros.
   split.
   unfold plain_length_check.
   destruct (leb (length (new_pwd pt)) len).
-  + simpl. *)
+  (* Case 1: Premise is false. *)
+  + simpl. congruence.
+  (* Case 2: Trivial. *)
+  + auto.
+  + unfold plain_length_check.
+    destruct (leb (length (new_pwd pt)) len).
+  - unfold is_true. simpl. congruence.
+  - unfold is_true. simpl. auto.
+Qed.
+  
 
 (* The new password must not contain more than a certain number of characters of
  * the same class in a row. *)
